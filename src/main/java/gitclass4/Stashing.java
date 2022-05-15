0e75540d3c54a78f9dd91bd05af3935fe05b45ef
yazgulesen <iamyazgul@gmail.com>
2022-05-15 11:03:19 -0400

    Basic structure of the car is ready,working in advanced features

---
+++ src/main/java/gitclass4/Stashing.java
@@ -0,0 +1,8 @@
+package gitclass4;
+
+public class Stashing {
+    public static void main(String[] args) {
+        System.out.println("hello stashing");
+        BasicFeatures car = new BasicFeatures();
+    }
+}

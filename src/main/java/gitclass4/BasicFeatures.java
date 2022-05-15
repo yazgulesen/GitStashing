193eb8bc4f4852098d6fc961ce25b833a8737565
yazgulesen <iamyazgul@gmail.com>
2022-05-15 12:05:20 -0400

    Red car is ready with basic features

--- src/main/java/gitclass4/BasicFeatures.java
+++ src/main/java/gitclass4/BasicFeatures.java
@@ -1,12 +1,12 @@
 package gitclass4;
 
 public class BasicFeatures {
-    public static void main(String[] args) {
+
         String engineCapac;
         String Color;
         String doors;
         String headlights;
         String regNo;
 
-    }
+
 }
--- src/main/java/gitclass4/Stashing.java
+++ src/main/java/gitclass4/Stashing.java
@@ -4,5 +4,11 @@ public class Stashing {
     public static void main(String[] args) {
         System.out.println("hello stashing");
         BasicFeatures car = new BasicFeatures();
+        car.Color="red";
+        car.doors="4";
+        car.engineCapac="V8";
+        car.headlights="blue";
+        car.regNo="tuzik";
+
     }
 }
